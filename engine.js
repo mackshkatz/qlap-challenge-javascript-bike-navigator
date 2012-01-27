611e5c8e7aae46bf03a8c0dbc0e505f1450d9400
Max Katz <maxwelljkatz@gmail.com>
2012-01-26 22:09:52 -0700

    Created a method called move that accepts two arguments, direction and distance. The method returns an array of the new lat and lng but the Bike instance doesn't save this new data. This method is attached to Bike's prototype.

--- engine.js
+++ engine.js
@@ -6,3 +6,29 @@ function Bike() {
 	this.lat = 40.017094;
 	this.lng = -105.283799;
 }
+
+function move(direction, miles) {
+	var direction = direction;
+	var degrees = miles / 69.172;
+	var lat = this.lat;
+	var lng = this.lng;
+	if ( direction == "north" ) 
+	{
+		lat += degrees;
+	}
+	else if ( direction == "south" ) 
+	{
+		lat -= degrees;
+	}
+	else if ( direction == "east" ) 
+	{
+		lng -= degrees;
+	}
+	else ( direction == "west" ) 
+	{
+		lng += degrees;
+	}
+	return [ lat, lng ];
+}
+
+Bike.prototype.move = move;
